86dcb3dcdd14e7d16b92674e8fa2140b0fc6ff62
Alejandro <agaviria1408@gmail.com>
2025-05-15 16:53:51 -0500

    Se realiza ejercicio para crear Menu y pagina para analisis de datos

---
+++ cliente_datos/src/components/pages/Login/Login.jsx
@@ -0,0 +1,19 @@
+export function Login(){
+    return(
+        <>
+            <section className="conatiner">
+                <section className="row justify-content-center">
+                    <section className="="col-6>
+                        <form className="p-5 border rounded shadow">
+                            <input type="text" className="form-control mb-3" placeholder="Estudiante"/>
+                            <input type="text" className="form-control mb-3" placeholder="Grupo"/>
+                            <input type="text" className="form-control mb-3" placeholder="Fecha"/>
+                            <button className="btn btn-primary">Enviar</button>
+                        </form>
+                    </section>
+                </section>
+            </section>
+        </>
+
+    )
+}
